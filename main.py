2dac134fade6cdb5cdc93e46e77b7432263e32a4
rayanrfd <dakhiarayan@gmail.com>
2025-08-05 23:37:44 +0100

    implemented travel plan extraction from user query

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-from schemas.schemas import TravelPlan
+from schemas.UserPlan import TravelPlan
 from tools.get_date_and_time import get_date_and_time_tool, get_date_and_time
 from dotenv import load_dotenv
 import os
@@ -8,54 +8,45 @@ import datetime
 
 load_dotenv()
 
-TOGETHER_API_KEY = os.getenv("TOGETHER_API_KEY")
-
 client = OpenAI(
-    api_key=os.environ.get("TOGETHER_API_KEY"),
-    base_url="https://api.together.xyz/v1",
+    api_key=os.environ.get("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 
 def extract_travel_plan(prompt):
-    messages=[
-            {"role": "system", "content": "Extract the event information."},
-            {"role": "user", "content": f"{prompt}, Answer in JSON"},
-        ],
-    
-    completion = client.chat.completions.create(
-        model="meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
-        messages=messages,
-        # response_format={
-        #         "type": "json_schema",
-        #         "schema": TravelPlan.model_json_schema(),
-        #     },
-        tools=[get_date_and_time_tool],
-        tool_choice="required",
-        )
 
-    tool_call = completion.choices[0].message.tool_calls[0]
+    messages=[
+            {"role": "system", "content": "Extract the travel information in a JSON format."},
+            {"role": "user", "content": f"{prompt}"},
+        ]
+    
+    
+    response = client.chat.completions.create(
+        model="gemini-2.0-flash",
+        messages=messages,
+        tools=get_date_and_time_tool,
+        tool_choice="required",
+    )
+    
+    tool_call = response.choices[0].message.tool_calls[0]
     args = json.loads(tool_call.function.arguments)
 
-    result = get_date_and_time(**args)
+    time_info = get_date_and_time(args["departure_tz"], args["destination_tz"])
 
-    messages.append(completion.choices[0].message)  # append model's function call message
+    messages.append(response.choices[0].message)  # append model's function call message
     messages.append({                               # append result message
         "role": "tool",
         "tool_call_id": tool_call.id,
-        "content": str(result)
+        "content": f"User time: {time_info['user_time']}, Destination time: {time_info['destination_time']}"
     })
 
-    completion_2 = client.chat.completions.create(
-        model="gpt-4.1",
+    completion = client.beta.chat.completions.parse(
+        model="gemini-2.0-flash",
         messages=messages,
-        tools=get_date_and_time_tool,
-        response_format={
-                "type": "json_schema",
-                "schema": TravelPlan.model_json_schema(),
-            },
+        response_format=TravelPlan,
     )
 
-    return completion_2.choices[0].message.content
-
+    return completion.choices[0].message.parsed.origin
 
 def main():
     print(extract_travel_plan("I want to travel from Paris to the lake Balkhash in Kazakstan in two days for a week."))
@@ -63,3 +54,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
